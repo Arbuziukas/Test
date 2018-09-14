bd37af4d79cf78ae364d0d6fa631ad5d119807a2
Julius <julius.rupsys98@gmail.com>
2018-09-14 12:19:56 +0300

    Test

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TestApp
+{
+    partial class window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(24, 12);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(167, 20);
+            this.nameField.TabIndex = 0;
+            this.nameField.TextChanged += new System.EventHandler(this.nameField_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Location = new System.Drawing.Point(219, 15);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(212, 42);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name appears here";
+            // 
+            // window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 346);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameField);
+            this.Name = "window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Window";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Label nameLabel;
+    }
+}
+
