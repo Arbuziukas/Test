46fdf6aab643ab5e5bd9fc7171c0137f82cb3be5
Julius Rupšys <julius.rupsys98@gmail.com>
2018-09-14 14:10:10 +0300

    asdsad

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TestApp
         [STAThread]
         static void Main()
         {
+//This is a comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new window());
